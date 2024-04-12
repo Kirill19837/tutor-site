aa3dd00b97a9e9548736dbd25ddc3294d2da67a2
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-04-12 13:17:03 +0300

    Change program cs

--- TutorPro/Program.cs
+++ TutorPro/Program.cs
@@ -16,16 +16,6 @@ WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
 
-if (app.Environment.IsDevelopment())
-{
-   
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseUmbraco()
     .WithMiddleware(u =>
