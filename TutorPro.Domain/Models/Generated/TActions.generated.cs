242d11424ee619cafd9c3bfb54a9c4717912f8fd
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-04-29 14:33:03 +0300

    create for actions

--- TutorPro.Domain/Models/Generated/TActions.generated.cs
+++ TutorPro.Domain/Models/Generated/TActions.generated.cs
@@ -90,6 +90,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		public virtual string TStyle => this.Value<string>(_publishedValueFallback, "tStyle");
 
 		///<summary>
+		/// Thanks action
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.2+79d241a")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("tThanksAction")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockGridModel TThanksAction => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockGridModel>(_publishedValueFallback, "tThanksAction");
+
+		///<summary>
 		/// Title
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.2+79d241a")]
